0cf7478b6c33079d3014553dae4e04803ba243d1
Nitin Garg <nitin992503@gmail.com>
2018-10-03 21:04:46 +0530

    all bugs fixed

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             console.log(schedule);
             let str='';
             for(let i =0 ;i<schedule.length;i++){
-                str+=`slot ${i+1}  ${schedule[i].date.toDateString()} ${getslot(schedule[i].slot)}  `;                
+                str+=`(slot ${i+1} . )  ${schedule[i].date.toDateString()} ${getslot(schedule[i].slot)}      `;                
             }
             agent.setContext({
                 'name' : 'fix',
@@ -104,7 +104,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                 'lifespan' : 5
             });
             console.log('done');
-            agent.add('please select a time slot for your appointment \n'+str);
+            agent.add('please select a time slot for your appointment '+str);
         })
         .catch(function(error) {
             console.log(error);
@@ -114,7 +114,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
 
     function fix(agent){
-        let p,q;
+        let p,q,id;
         request.body.queryResult.outputContexts.forEach(ele => {
             if(ele.name == 'projects/gea-xssuiw/agent/sessions/e0170c4e-572c-5073-d91a-92314a9f23a5/contexts/appliance')
             p = ele.parameters;
@@ -127,6 +127,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         console.log(typeof(slot));
         console.log(q[slot]);
         console.log(parseInt(q[slot].slot));
+        let da = new Date(q[slot].date);
+        
         return db.collection("users").add({
             email : p.email,
             problem : p.problem,
@@ -137,11 +139,17 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
             model : p.model_no,
             appliance : p.appliance,
             time : getslot(parseInt(q[slot].slot)),
-            date : q[slot].date,
+            date : da.toDateString(),
             technician : q[slot].technician,
+            slot : q[slot].slot,
         })
         .then(usr=>{
+            id = usr.id;
             agent.add('Your appointment is booked , your service id is '+usr.id);
+            let tech = db.collection('technician');
+            return tech.doc(q[slot].technician).get();
+        })
+        .then(doc=>{
             let p,q;
             request.body.queryResult.outputContexts.forEach(ele => {
                 if(ele.name == 'projects/gea-xssuiw/agent/sessions/e0170c4e-572c-5073-d91a-92314a9f23a5/contexts/appliance')
@@ -150,29 +158,25 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                 q = ele.parameters.schedule;
             });
             let slot = parseInt(p.slot)-1;
+            let da = new Date( q[slot].date);
+            let dat = da.toDateString();
             let tech = db.collection('technician');
-            tech.doc(q[slot].technician).get()
-            .then(doc=>{
-                let d = doc.data();
-                if(d[q[slot].date]){
-                    let s = d[q[slot].date]; 
-                    s[parseInt(q[slot].slot)]=usr.id;
-                    let obj = {};
-                    obj[q[slot].date] = s;
-                    return tech.doc(doc.id).update(obj);
-                }
-                else{
-                    let s = ['n','n','n'];
-                    s[q[p.slot-1].slot] = usr.id;
-                    let obj = {};
-                    obj[q[p.slot-1].date] = s;
-                    return tech.doc(doc.id).update(obj);
-                }
-            })
-            .catch(err => console.log(err));
-        })
-        .then(doc=>{
-            return db.collection("user_no").doc(doc.id).update({number : doc.data().number+1});
+            console.log('success');
+            let d = doc.data();
+            if(d[dat]){
+                let s = d[dat]; 
+                s[parseInt(q[slot].slot)]=id;
+                let obj = {};
+                obj[dat] = s;
+                return tech.doc(doc.id).update(obj);
+            }
+            else{
+                let s = ['n','n','n'];
+                s[q[slot].slot] = id;
+                let obj = {};
+                obj[dat] = s;
+                return tech.doc(doc.id).update(obj);
+            }
         })
         .then(doc=>{
             console.log('success');
@@ -184,15 +188,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
     function scheduler(technician){
         let date = new Date();
-        date.setHours(8);
+        console.log(date);
         let slot = 0;
         let book = [];
         let schdld = date;
-                
         if(date.getHours() < 9){
             for(let i=0;i<3;i++)
                 for(let prop in technician){
-                    if(technician[prop].hasOwnProperty(date.toDateString())){
+                    if(technician[prop].hasOwnProperty(date)){
                         if(technician[prop][date.toDateString()][i] == 'n' && technician[prop][date.toDateString()][i] );   
                         else{
                             book.push({
@@ -218,7 +221,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         else if(date.getHours() < 12){
             for(let i=1;i<3;i++)
                 for(let prop in technician){
-                    if(technician[prop].hasOwnProperty(date.toDateString())){
+                    if(technician[prop].hasOwnProperty(date)){
                         if(technician[prop][date.toDateString()][i] =='n' && technician[prop][date.toDateString()][i] );   
                         else{
                             book.push({
@@ -310,109 +313,147 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
     
     function deleteap(agent){
-        return db.collection('users').doc(agent.parameters.id).delete().then(doc=>{
+        let usr = db.collection('users').doc(agent.parameters.id);
+        let tec,date,slot,col;
+        return usr.get()
+        .then(doc=>{
+            if(doc.exists){
+                slot = doc.data().slot;
+                date = doc.data().date;
+                tec = doc.data().technician;
+                col = db.collection('technician').doc(tec);
+                return db.collection('users').doc(agent.parameters.id).delete();
+            }
+            else{
+                let err = 'no appointment with this service id found';
+                throw err;
+            }
+        })
+        .then(doc=>{
             agent.add('appointment canceled successfully');
-        }).catch(err => console.log(err));
+            return col.get();
+        })
+        .then(doc=>{
+            console.log(doc.data);
+            let arr = doc.data()[date];
+            console.log(typeof(slot));
+            console.log(arr);
+            arr[slot] = 'n';
+            let obj = {};
+            obj[date] = arr;
+            return col.update(obj);
+        })
+        .then(doc => {
+            console.log('success');
+            return 0;
+        })
+        .catch(err => {
+            console.log(err);
+            return err;
+        });
     }
     
     function reschedule(agent){
         let id = agent.parameters.id;
-        let usr;
-        db.collection('users').doc(id).get()
+        let usr,pd,slot;
+        return db.collection('users').doc(id).get()
         .then(doc=>{
             usr = doc.data();
-            return db.collection('pincode').doc(usr.pincode).get()
+            return db.collection('pincode').doc(usr.pincode).get();
+        })
         .then(doc => {
             if(doc.exists){
                 // scheduler 
                 let tech = [];
-                console.log(doc.data());
                 doc.data().tech.forEach(ele=>tech.push(db.doc('technician/'+ele)));
-                console.log(tech);
                 //push details of techinicians in array
                 return db.getAll(...tech);
             }
             else{
                 let a = 'services at pincode are not yet avilable';
-                console.log(a);
                 throw a;
             }
         })
         .then(tec=>{
-            console.log(tec);
             let technician = {};
             tec.forEach(ele=>{
                     let a = ele.id;
                     technician[a]=ele.data();
                 });
-            console.log(technician);
             //schedule appointment
             let schedule = scheduler(technician);
-            console.log(schedule);
             let str='';
             for(let i =0 ;i<schedule.length;i++){
-                str+=`slot ${i+1}  ${schedule[i].date.toDateString()} ${getslot(schedule[i].slot)}  `;                
+                str+=` (slot ${i+1}. )  ${schedule[i].date.toDateString()} ${getslot(schedule[i].slot)}  `;                
             }
             agent.setContext({
                 'name' : 'fix2',
                 'parameters' : {
                     'schedule' : schedule,
+                    'id' : id,
+                    'prev_date' : usr.date,
+                    'prev_slot' : usr.slot,
                 },
                 'lifespan' : 5
             });
-            console.log('done');
-            agent.add('please select a time slot for your appointment \n'+str);
+            agent.add('please select a time slot for your appointment '+str);
+            return 0;
         })
         .catch(function(error) {
             console.log(error);
             agent.add("Cannot book service appointment: "+error);
         });
-        });
     }
 
     function fix2(agent){
-        let p;
+        let p,id;
         request.body.queryResult.outputContexts.forEach(ele => {
             if(ele.name == 'projects/gea-xssuiw/agent/sessions/e0170c4e-572c-5073-d91a-92314a9f23a5/contexts/fix2')
-            p = ele.parametrs;
+            p = ele.parameters;
         });
+        id = p.id;
         let slot =  parseInt(p.slot)-1;
+        let da = new Date(p.schedule[slot].date);
         return db.collection("users").doc(p.id).update({
-            time : getslot(parseInt(p[slot].slot)),
-            date : p[slot].date,
-            technician : p[slot].technician,
+            time : getslot(parseInt(p.schedule[slot].slot)),
+            date : da.toDateString(),
+            technician : p.schedule[slot].technician,
+            slot : parseInt(p.schedule[slot].slot),
         })
         .then(usr=>{
-            agent.add('Your appointment is successfully updated ');
-            let p;
-            request.body.queryResult.outputContexts.forEach(ele => {
-                if(ele.name == 'projects/gea-xssuiw/agent/sessions/e0170c4e-572c-5073-d91a-92314a9f23a5/contexts/fix2')
-                p = ele.parameters;
-            });
-            let slot = parseInt(p.slot)-1;
             let tech = db.collection('technician');
-            tech.doc(p.schedule[slot].technician).get()
-            .then(doc=>{
-                let d = doc.data();
-                if(d[p.schedule[slot].date]){
-                    let s = d[p.schedule[slot].date]; 
-                    s[parseInt(p.schedule[slot].slot)]=usr.id;
-                    let obj = {};
-                    obj[p.schedule[slot].date] = s;
-                    return tech.doc(doc.id).update(obj);
-                }
-                else{
-                    let s = ['n','n','n'];
-                    s[p.schedule[p.slot-1].slot] = usr.id;
-                    let obj = {};
-                    obj[p.schedule[p.slot-1].date] = s;
-                    return tech.doc(doc.id).update(obj);
-                }
-            })
-            .catch(err => console.log(err));
+            agent.add('Your appointment is successfully updated ');
+            return tech.doc(p.schedule[slot].technician).get();
         })
         .then(doc=>{
-            return db.collection("user_no").doc(doc.id).update({number : doc.data().number+1});
+            let d = doc.data();
+            console.log(d);
+            let tech = db.collection('technician');
+            let obj = {};
+            if(da.toDateString() == p.prev_date && d[da.toDateString()]){
+                let s = d[da.toDateString()]; 
+                s[parseInt(p.schedule[slot].slot)]= id;
+                console.log(typeof(p.prev_slot));
+                console.log(s);
+                s[p.prev_slot] = 'n';
+                obj[da.toDateString()] = s;
+                console.log(s);
+                console.log(obj);
+                return tech.doc(doc.id).update(obj);
+            }
+            else{
+                let s1 = ['n','n','n'] , s2;
+                s1[p.schedule[slot].slot] = id;
+                s2 = d[p.prev_date];
+                console.log(typeof(p.prev_slot));
+                console.log(s1);
+                console.log(s2);
+                s2[p.prev_slot]='n';
+                obj[da.toDateString()] = s1;
+                obj[p.prev_date] = s2;
+                console.log(obj);
+                return tech.doc(doc.id).update(obj);
+            }
         })
         .then(doc=>{
             console.log('success');
@@ -425,9 +466,18 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     function info(){
         return db.collection('users').doc(agent.parameters.id).get()
         .then(usr=>{
-            agent.add(`information ${usr.data}`);
+            console.log(usr.data());
+            let inf = usr.data();
+            let date = inf.date;
+            let time = inf.time;
+            let tec = inf.technician;
+            let app = inf.appliance;
+            let model = inf.model;
+            let serial = inf.serial;
+            agent.add(`information :- | date : ${date} , time : ${time} , technicain : ${tec} , appliance : ${app} , model no. ${model} , serial no. : ${serial}`);          
         });
     }
+    
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
     intentMap.set('cancel - appointment - custom' , deleteap);
@@ -436,8 +486,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     intentMap.set('book appointment - custom - yes - custom - custom',fix);
     intentMap.set('Default Welcome Intent', welcome);
     intentMap.set('Default Fallback Intent', fallback);
-    intentMap.set('reschedule - appointment - custom', reschedule);
-    intentMap.set('reschedule - appointment - custom - custom',fix2);
+    intentMap.set('reschedule - custom', reschedule);
+    intentMap.set('reschedule - custom - custom',fix2);
     intentMap.set('info - custom',info);
     agent.handleRequest(intentMap);
 });
